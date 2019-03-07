bb037be3c1a44514767bf26e9bc7b7683c21ad57
Max Goodman <max.goodman@flatironschool.com>
2019-03-07 11:01:38 -0500

    adding styling updates

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :restaurants
   get 'pages/about'
   get 'pages/contact'
-  
+  root 'restaurants#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
